3075a6a68d9bf3f09552f34a1145ce2026321065
0D1Um3 <125272910+0D1Um3@users.noreply.github.com>
2023-11-29 22:45:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
+var but1 = document.getElementById('navigation__item-1');
+var but2 = document.getElementById('navigation__item-2');
+var text = document.getElementById('text_book');
+var reviews = document.getElementById('reviews');
+var underline = document.getElementById('underline');
+
+but1.style.color = 'white';
+
+function setUnderline(element) {
+    var elementRect = element.getBoundingClientRect();
+    underline.style.width = elementRect.width + 'px';
+    underline.style.transform = 'translateX(' + (elementRect.left - but1.offsetLeft) + 'px)';
+}
+
+setTimeout(function () {
+    setUnderline(but1);
+}, 0);
+
+but1.addEventListener('click', function () {
+    if (but2.style.color === 'white') {
+        reviews.style.display = 'none';
+        text.style.display = 'flex';
+        but1.style.color = 'white';
+        but2.style.color = '#959595';
+
+        // Устанавливаем новые размеры и позицию для underline
+        setTimeout(function () {
+            setUnderline(but1);
+        }, 0);
+    }
+});
+
+but2.addEventListener('click', function () {
+    if (but1.style.color === 'white') {
+        reviews.style.display = 'block';
+        text.style.display = 'none';
+        but2.style.color = 'white';
+        but1.style.color = '#959595';
+
+        // Устанавливаем новые размеры и позицию для underline
+        setTimeout(function () {
+            setUnderline(but2);
+        }, 0);
+    }
+});
+
+
 
